ee07fe0476307966ff8160282097a019f42b3cc1
Kiel Sprague <aereisdin@gmail.com>
2021-03-24 14:28:07 -0700

    Working on routes for Product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,9 +5,9 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try{
-    const categoryData = await Category.findAll({
-      include: [{ model: Product}]
-    })
+    const categoryData = await Category.findAll(
+      
+    )
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err)
@@ -48,16 +48,20 @@ router.post('/', async (req, res) => {
 });
 
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
+    const categoryData = await Category.update(
+      {category_name: req.body.category_name},
+      {where: req.params.id}
+    )
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
