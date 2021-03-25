b82bc45649599f6497d68c204e9f20afa8db1d42
Kiel Sprague <aereisdin@gmail.com>
2021-03-25 13:08:04 -0700

    error checking

--- models/index.js
+++ models/index.js
@@ -18,13 +18,15 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
-  foreignKey: 'tag_id'
+  as: 'tag_product',
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'ProductTag',
-  foreignKey: 'product_id',
+  as: 'product_tag',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,10 +6,10 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try{
     const tagData = await Tag.findAll(
-      {include: [{model: Product}] 
+      {include: [{model: Product, through: ProductTag, as: 'product_tag'} ] 
       })
     res.status(200).json(tagData);
-  } catch (err) {
+  } catch (err) { 
     res.status(500).json(err)
   }
 });
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{model: Product, through: ProductTag, as: 'product_tag'}],
     });
 
     if (!tagData) {
