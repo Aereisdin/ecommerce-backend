1018e48384e57b3120b3df0c525817719921f4d4
Kiel Sprague <aereisdin@gmail.com>
2021-03-25 11:21:38 -0700

    Added Tag Routes, forgot about those lol

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,8 +6,8 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try{
     const categoryData = await Category.findAll(
-      
-    )
+      {include: [{model: Product}] 
+    })
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err)
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product }]
     });
 
     if (!categoryData) {
